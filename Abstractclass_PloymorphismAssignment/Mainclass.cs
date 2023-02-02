f4fce7b4cb6092cabd2bc4fd1ebfb0b7a5ef13af
Abinaya <88155924+Abinayakalvirajan@users.noreply.github.com>
2023-02-02 17:37:06 +0000

    Add files via upload

---
+++ Abstractclass_PloymorphismAssignment/Mainclass.cs
@@ -0,0 +1,40 @@
+﻿using System;
+abstract class Phone
+{ 
+    public abstract void Show();
+}
+class Iphone : Phone
+{
+    public override void Show()
+    {
+        Console.WriteLine("Good !");
+    }
+}
+class Vivo : Phone
+{
+    public override void Show()
+    {
+        Console.WriteLine("Only for Speak!");
+    }
+}
+class Oneplus : Phone
+{
+    public override void Show()
+    {
+        Console.WriteLine("Camera Quality!");
+    }
+}
+class MainClass
+{
+    static void Main()
+    {
+        Phone[] s = new Phone[3];
+        s[0] = new Iphone();
+        s[1] = new Vivo();
+        s[2] = new Oneplus();
+        foreach (Phone data in s)
+        {
+            data.Show();
+        }
+    }
+}
